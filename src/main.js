24556a9812286035eb45e86c14a732264299e87a
TamM619 <mateusenrique123@gmail.com>
2025-06-11 13:12:03 -0300

    Cabeçalho finalizado

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]')
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    //Cabeçalho
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        } else{
+            exibeElementosdoHeader();
+        }
+    })
+
+//Atrações, abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,10 +28,20 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    //Seção FAQ
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta)
     }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is--hidden');
+}
+function exibeElementosdoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is--hidden');
+}
 
 function abreOuFechaResposta(elemento)
 {
